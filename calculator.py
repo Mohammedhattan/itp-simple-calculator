16546221790d5085948e0efd9c9a3cc2d26d8c23
Mohammedhattan <hattan-100@hotmail.com>
2024-07-03 10:39:12 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,33 @@
+import math
 def add(x, y):
     return x + y
 
 
 def subtract(x, y):
+    return x-y
     pass
 
 
 def divide(x, y):
+    return x/y
     pass
 
 
 def multiply(x, y):
+    return x*y
     pass
 
 
 def square(x):
+    return ^x
     pass
 
 
 def power(x, y):
+    x**y
     pass
 
 
 def sqrt(x):
+    return math.sqrt(x)
     pass
